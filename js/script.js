90837da465ee33c16149e74965c511cf1d61fabb
kokeGG <kokehr99@gmail.com>
2022-07-09 02:14:56 -0500

    font size

--- js/script.js
+++ js/script.js
@@ -19,7 +19,24 @@ let cwidth, cheight;
 let shells = [];
 let pass= [];
 
-let colors = ['#FF5252', '#FF4081', '#E040FB', '#7C4DFF', '#536DFE', '#448AFF', '#40C4FF', '#18FFFF', '#64FFDA', '#69F0AE', '#B2FF59', '#EEFF41', '#FFFF00', '#FFD740', '#FFAB40', '#FF6E40'];
+let colors = [
+    '#FF5252', 
+    '#FF4081', 
+    '#E040FB',
+    '#7C4DFF',
+    '#536DFE',
+    '#448AFF',
+    '#40C4FF',
+    '#18FFFF',
+    '#64FFDA',
+    '#69F0AE',
+    '#B2FF59',
+    '#EEFF41',
+    '#FFFF00',
+    '#FFD740',
+    '#FFAB40',
+    '#FF6E40'
+];
 
 window.onresize = function() { reset(); }
 reset();
